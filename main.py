d746f76bf371eb96c4b2afe1d0a7b94bc9f4e32b
pingkka <suyun1999@naver.com>
2021-02-13 15:03:05 +0900

    merge files

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+# pip install TensorFlow
 from flask import Flask, request  # 서버 구현을 위한 Flask 객체 import
 from flask_restx import Api, Resource  # Api 구현을 위한 Api 객체 import
+import sent
+import split_txt
 
 app = Flask(__name__)  # Flask 객체 선언, 파라미터로 어플리케이션 패키지의 이름을 넣어줌.
 api = Api(app)  # Flask 객체에 Api 객체 등록
@@ -25,6 +28,12 @@ def name():
     return {name: id}
 
 
+@app.route('/test', methods=['GET'])
+def test():
+    return sent.test()
+
+
 if __name__ == "__main__":
+    split_txt.split_txt()
     app.config['JSON_AS_ASCII'] = False
     app.run(debug=True, host='0.0.0.0', port=8080)
